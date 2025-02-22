aee233ec5954f5e690f12d79f9a131c79dca6b76
ncory44 <ncory44@gmail.com>
2025-02-21 21:17:12 -0800

    adding js to html and css video pt 2

---
+++ Simple-Recipes/JavaScript/javascript.js
@@ -0,0 +1,29 @@
+//RECIPE POP UP MODAL SECTION
+
+//Sets up the button that will open the recipe modal
+var btns = document.querySelectorAll("input.modal-button");
+
+//Defines all modals for each recipe
+var modals = document.querySelectorAll(".recipe-modal");
+
+//get the <span> element that closes the modal
+var closeBtn = document.getElementsByClassName("close-btn");
+
+//when the user clicks the recipe button, open the modal
+for (var i = 0; i < btns.length; i++){
+    btns[i].onclick = function(event){
+        modal = document.querySelector(event.target.getAttribute("href"));
+        modal.style.display = "block";
+    }
+}
+
+//when the user clicks on <span> (x), close the modal
+for (var i = 0; i < closeBtn.length; i++) {
+    closeBtn[i].onclick = function() {
+        for (var index in modals){
+            if (modals[index].style) {
+                modals[index].style.display = "none";
+            }
+        }
+    }
+}
